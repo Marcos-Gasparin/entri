97585aaa0bb5cb37c77a7c3a494c6afa55f8a717
Marcos-Gasparin <msgasparin30@gmail.com>
2025-03-18 19:20:38 -0300

    Delete test.js
    Unecessary file

--- test.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-async function testFetch() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const data = await response.json();
-    console.log(data);
-}
-
-testFetch();
